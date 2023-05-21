9c829c4a98b9de6180565522735fd3c1683eaa50
Vinicios Luneburger Anacleto <viniciosanacleto@hotmail.com>
2023-05-21 18:33:45 -0300

    feat: a320 checklist (#1)
    Co-authored-by: Vinicios Anacleto <vinicios.anacleto@mosaico.com.br>

--- src/assets/checklists/index.ts
+++ src/assets/checklists/index.ts
@@ -1,10 +1,12 @@
 import Cessna152 from "./aircrafts/cessna-152";
 import CessnaCitationCJ4 from "./aircrafts/cessna-citation-cj4";
+import AirbusA320 from "./aircrafts/airbus-a320-a32nx";
 import { Checklist } from "./type";
 
-const Checklists: {[id: string]: Checklist} = {
+const Checklists: { [id: string]: Checklist } = {
   "cessna-152": Cessna152,
   "cessna-citation-cj4": CessnaCitationCJ4,
-}
+  "airbus-a320-a32nx": AirbusA320,
+};
 
-export default Checklists
+export default Checklists;
