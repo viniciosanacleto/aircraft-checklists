269df6e3696b95843a1d62899f2d171d20b84894
Vinicios Luneburger Anacleto <viniciosanacleto@hotmail.com>
2023-05-26 16:22:11 -0300

    feat: order manufacturers alphabetically in menu

--- src/widgets/AircraftsMenu/AircraftsMenu.tsx
+++ src/widgets/AircraftsMenu/AircraftsMenu.tsx
@@ -5,13 +5,13 @@ import style from "./AircraftsMenu.module.scss";
 import AircraftChecklists from "../../assets/checklists";
 import classNames from "classnames";
 import { slugify } from "@/utils/slugify";
-
-export interface AircraftsMenuProps{
-  className?: string
+import { sortObjByKeyAlphabetically } from "@/utils/sort";
+export interface AircraftsMenuProps {
+  className?: string;
 }
 
-export default function AircraftsMenu({className}: AircraftsMenuProps) {
-  const defaultClassNames = classNames(className, style.AircraftsMenu)
+export default function AircraftsMenu({ className }: AircraftsMenuProps) {
+  const defaultClassNames = classNames(className, style.AircraftsMenu);
 
   const acftByManufacturer: {
     [key: string]: { key: string; model: string }[];
@@ -31,42 +31,53 @@ export default function AircraftsMenu({className}: AircraftsMenuProps) {
   return (
     <div className={defaultClassNames}>
       <ul className={style.ManufacturersList}>
-        {Object.keys(acftByManufacturer).map((manufacturer) => {
-          return (
-            <li className={style.Manufacturer} key={`manufacturer-${slugify(manufacturer)}`}>
-              <input type="checkbox" id={`manufacturer-${slugify(manufacturer)}`} />
-              <label htmlFor={`manufacturer-${slugify(manufacturer)}`}>
-                {manufacturer}
-                <Image
-                  src="img/icons/chevron-down.svg"
-                  alt={`${manufacturer} menu`}
-                  width={20}
-                  height={20}
-                  className={style.Chevron}
+        {Object.keys(sortObjByKeyAlphabetically(acftByManufacturer)).map(
+          (manufacturer) => {
+            return (
+              <li
+                className={style.Manufacturer}
+                key={`manufacturer-${slugify(manufacturer)}`}
+              >
+                <input
+                  type="checkbox"
+                  id={`manufacturer-${slugify(manufacturer)}`}
                 />
-              </label>
+                <label htmlFor={`manufacturer-${slugify(manufacturer)}`}>
+                  {manufacturer}
+                  <Image
+                    src="img/icons/chevron-down.svg"
+                    alt={`${manufacturer} menu`}
+                    width={20}
+                    height={20}
+                    className={style.Chevron}
+                  />
+                </label>
 
-              <ul className={style.AircraftsList}>
-                {acftByManufacturer[manufacturer].map((aircraft) => {
-                  return (
-                    <li className={style.Aircraft} key={`acft-${aircraft.key}`}>
-                      <Link href={`/checklist/${aircraft.key}`}>
-                        <Image
-                          src="img/icons/airplane-right.svg"
-                          alt="Expand"
-                          width={22}
-                          height={22}
-                          className={style.Icon}
-                        />
-                        {aircraft.model}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </li>
-          );
-        })}
+                <ul className={style.AircraftsList}>
+                  {acftByManufacturer[manufacturer].map((aircraft) => {
+                    return (
+                      <li
+                        className={style.Aircraft}
+                        key={`acft-${aircraft.key}`}
+                      >
+                        <Link href={`/checklist/${aircraft.key}`}>
+                          <Image
+                            src="img/icons/airplane-right.svg"
+                            alt="Expand"
+                            width={22}
+                            height={22}
+                            className={style.Icon}
+                          />
+                          {aircraft.model}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          }
+        )}
       </ul>
     </div>
   );
