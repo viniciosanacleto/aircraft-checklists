fe38fd6bd2a39235742570f438ea0490be52f78a
Vinicios Anacleto <vinicios.anacleto@mosaico.com.br>
2023-10-26 23:06:59 -0300

    feat: cirrus vision jet sf50 checklist

--- src/assets/checklists/index.ts
+++ src/assets/checklists/index.ts
@@ -1,12 +1,14 @@
 import Cessna152 from "./aircrafts/cessna-152";
 import CessnaCitationCJ4 from "./aircrafts/cessna-citation-cj4";
 import AirbusA320 from "./aircrafts/airbus-a320-a32nx";
+import CirrusSF50 from "./aircrafts/cirrus-vision-sf50";
 import { Checklist } from "./type";
 
 const Checklists: { [id: string]: Checklist } = {
   "cessna-152": Cessna152,
   "cessna-citation-cj4": CessnaCitationCJ4,
   "airbus-a320-a32nx": AirbusA320,
+  "cirrus-sf50": CirrusSF50,
 };
 
 export default Checklists;
