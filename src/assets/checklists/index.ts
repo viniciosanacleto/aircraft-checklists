b651de712e38a156cb863e92abe3219eb6bab97c
Vinicios Luneburger Anacleto <viniciosanacleto@hotmail.com>
2023-11-02 01:19:10 -0300

    feat: create boeing 737 700/800 checklist
    * feat: create boeing 737 700/800 checklist
    
    * Update index.ts

--- src/assets/checklists/index.ts
+++ src/assets/checklists/index.ts
@@ -2,6 +2,7 @@ import Cessna152 from "./aircrafts/cessna-152";
 import CessnaCitationCJ4 from "./aircrafts/cessna-citation-cj4";
 import AirbusA320 from "./aircrafts/airbus-a320-a32nx";
 import CirrusSF50 from "./aircrafts/cirrus-vision-sf50";
+import Boeing738 from "./aircrafts/boeing-738";
 import { Checklist } from "./type";
 
 const Checklists: { [id: string]: Checklist } = {
@@ -9,6 +10,7 @@ const Checklists: { [id: string]: Checklist } = {
   "cessna-citation-cj4": CessnaCitationCJ4,
   "airbus-a320-a32nx": AirbusA320,
   "cirrus-sf50": CirrusSF50,
+  "boeing-738": Boeing738
 };
 
 export default Checklists;
