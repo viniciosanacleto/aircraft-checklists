4f5a8efa234cb52bc15e1163dd0928a8b24b9834
Vinicios Luneburger Anacleto <viniciosanacleto@hotmail.com>
2024-01-07 14:40:31 -0300

    feat: learjet35 checklist

--- src/assets/checklists/index.ts
+++ src/assets/checklists/index.ts
@@ -3,6 +3,7 @@ import CessnaCitationCJ4 from "./aircrafts/cessna-citation-cj4";
 import AirbusA320 from "./aircrafts/airbus-a320-a32nx";
 import CirrusSF50 from "./aircrafts/cirrus-vision-sf50";
 import Boeing738 from "./aircrafts/boeing-738";
+import Learjet35 from "./aircrafts/learjet-35";
 import { Checklist } from "./type";
 
 const Checklists: { [id: string]: Checklist } = {
@@ -11,6 +12,7 @@ const Checklists: { [id: string]: Checklist } = {
   "airbus-a320-a32nx": AirbusA320,
   "cirrus-sf50": CirrusSF50,
   "boeing-738": Boeing738
+  "learjet-35": Learjet35
 };
 
 export default Checklists;
