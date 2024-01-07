7617bceaf57a8bc518c94a1277e956f0fbf810de
Vinicios Luneburger Anacleto <viniciosanacleto@hotmail.com>
2024-01-07 15:21:34 -0300

    Update index.ts

--- src/assets/checklists/index.ts
+++ src/assets/checklists/index.ts
@@ -4,6 +4,7 @@ import AirbusA320 from "./aircrafts/airbus-a320-a32nx";
 import CirrusSF50 from "./aircrafts/cirrus-vision-sf50";
 import Boeing738 from "./aircrafts/boeing-738";
 import Learjet35 from "./aircrafts/learjet-35";
+import AirbusA300600 from "./aircrafts/airbus-a300-600";
 import { Checklist } from "./type";
 
 const Checklists: { [id: string]: Checklist } = {
@@ -12,7 +13,8 @@ const Checklists: { [id: string]: Checklist } = {
   "airbus-a320-a32nx": AirbusA320,
   "cirrus-sf50": CirrusSF50,
   "boeing-738": Boeing738,
-  "learjet-35": Learjet35
+  "learjet-35": Learjet35,
+  "airbus-a300-600": AirbusA300600
 };
 
 export default Checklists;
